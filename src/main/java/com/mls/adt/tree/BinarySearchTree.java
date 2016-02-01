7fca0746eecf5d7478df3ab4e3416b5b01e7f904
唐少雄 <tangshaoxiong@tangshaoxiong-mbp.local>
2016-02-01 23:59:28 +0800

    二叉树删除

--- src/main/java/com/mls/adt/tree/BinarySearchTree.java
+++ src/main/java/com/mls/adt/tree/BinarySearchTree.java
@@ -15,25 +15,7 @@ public class BinarySearchTree<T> {
 
 	void insert(T v) {
 		PtrBinNode<T> node = new PtrBinNode<T>(v);
-		inner_insert(root, node);
-	}
-
-	private void inner_insert(BinaryNode<T> p, PtrBinNode<T> node) {
-		if (comp(node.val(), p.val()) < 0) {
-			if (p.left() == null) {
-				p.setLeft(node);
-				return;
-			}
-			inner_insert(p.left(), node);
-		} else if (comp(node.val(), p.val()) > 0) {
-			if (p.right() == null) {
-				p.setRight(node);
-				return;
-			}
-			inner_insert(p.right(), node);
-		} else { // 相同值节点处理： 只出现在右子树中
-			// TODO
-		}
+		_insert(root, node);
 	}
 
 	boolean isExsits(T val) {
@@ -42,19 +24,25 @@ public class BinarySearchTree<T> {
 		return _isExsits(root, val);
 	}
 
-	private boolean _isExsits(BinaryNode<T> node, T v) {
-		if (node == null)
-			return false;
-		if (comp(v, node.val()) < 0)
-			return _isExsits(node.left(), v);
-		else if (comp(v, node.val()) > 0)
-			return _isExsits(node.right(), v);
-		else
-			return true;
+	// 情况1： 删除节点有没有子树-直接删除
+	// 情况2： 删除节点有一个左（右）子树，提升子树节点
+	// 情况3： 删除节点存在左右两个子树-使用右子树中最小值代替当前节点，删除右子树中最小节点
+	void delete(BinaryNode<T> node) {
+		
 	}
 	
-	void delete(BinaryNode<T> node) {
-
+	
+	private BinaryNode<T> deleteMin(BinaryNode<T> node, BinaryNode<T> min) {
+		if (node == null) {
+			return null;
+		}
+		if (node.left() == null) {
+			min = node;
+			return node.right();
+		} else {
+			node.setLeft(deleteMin(node.left(), min));
+			return node;
+		}
 	}
 
 	void print() { // 层次打印
@@ -81,6 +69,35 @@ public class BinarySearchTree<T> {
 		return cp.compare(o1, o2);
 	}
 	
+	private boolean _isExsits(BinaryNode<T> node, T v) {
+		if (node == null)
+			return false;
+		if (comp(v, node.val()) < 0)
+			return _isExsits(node.left(), v);
+		else if (comp(v, node.val()) > 0)
+			return _isExsits(node.right(), v);
+		else
+			return true;
+	}
+	
+	private void _insert(BinaryNode<T> p, PtrBinNode<T> node) {
+		if (comp(node.val(), p.val()) < 0) {
+			if (p.left() == null) {
+				p.setLeft(node);
+				return;
+			}
+			_insert(p.left(), node);
+		} else if (comp(node.val(), p.val()) > 0) {
+			if (p.right() == null) {
+				p.setRight(node);
+				return;
+			}
+			_insert(p.right(), node);
+		} else { // 相同值节点处理： 只出现在右子树中
+			// TODO
+		}
+	}
+	
 	public static void main(String[] args) {
 //		BinaryNode<Integer> nodeA = new PtrBinNode<Integer>(37);
 //		BinaryNode<Integer> nodeB = new PtrBinNode<Integer>(24);
@@ -96,8 +113,14 @@ public class BinarySearchTree<T> {
 		bst.insert(24);
 		bst.insert(42);
 		bst.insert(32);
-		bst.insert(7);
-		System.out.println(bst.isExsits(20));
-		// bst.print();
+		PtrBinNode node = new PtrBinNode<Integer>(0);
+		System.out.println(bst.deleteMin(bst.root,node));
+		System.out.println(node.val());
+//		bst.insert(7);
+//		bst.insert(2);
+//		bst.insert(40);
+//		bst.insert(120);
+		// System.out.println(bst.isExsits(20));
+		bst.print();
 	}
 }
